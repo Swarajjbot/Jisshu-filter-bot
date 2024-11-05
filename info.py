ceaf9de7ccfb3214df13402dde7caaa8abb2d64c
Swarajjbot <165831838+Swarajjbot@users.noreply.github.com>
2024-11-05 22:14:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,9 +19,9 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1967990132').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/IM_JISSHU") # ADMIN USERNAME
+USERNAME = environ.get('USERNAME', "https://t.me/Heisenberg1609") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002202094211'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/JisshuMovieZone')
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/Heisenberg1609')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002253343935').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
